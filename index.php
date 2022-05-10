56b9370f7baaffe2f2debd7f0c348162406fe4c7
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-10 11:42:29 +0530

    Write a PHP program to filter out some elements with certain key-names.

--- index.php
+++ index.php
@@ -1,32 +1,24 @@
 <?php
-  $arr=array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+  $arr1=array('c1' => 'Red', 'c2' => 'Green', 'c3' => 'White', 'c4' => 'Black');
+  $arr2=array('c4','c2');
   
-  
+  foreach($arr1 as $key => $val){
+      if((array_search($key, $arr2))===FALSE){
+          
+          $res[$key]=$val;
+
+      }
+  }
 
 
-   echo("Recorded Temperatures");
-   for($i=0;$i<count($arr);$i++)
-        echo("$arr[$i] ");
-        echo ("<br><br>");
+   echo("Output : <br>");
+   foreach($res as $key => $val){
+       echo("$key :  $val <br>");
+   
+   }
+   echo ("<br><br>");
 
    
-   $avg =array_sum($arr)/count($arr);
-        echo("Average Temperature is: $avg");
-      
-        echo ("<br><br>");
-
-   sort($arr);
-        echo("5 Lowest Temperatures");
-        for($i=0;$i<5;$i++)
-        echo("$arr[$i] ");
-        echo ("<br><br>");
-    
-        rsort($arr);
-        echo("5 Highest Temperatures");
-        for($i=0;$i<5;$i++)
-        echo("$arr[$i] ");
-        echo ("<br><br>");
-
 
 
 
